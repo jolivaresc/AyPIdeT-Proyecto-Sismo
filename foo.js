5004337b58fb207ea14d5b7bb18c5fdb6428cc67
José Castillo <o.castillojl@gmail.com>
2017-10-17 19:47:27 -0500

    Función para buscar por palabras (ciudades)
    Se añadió un pequeño formulario en donde se ingresa una palabra y al hacer click en el botón search se dispara un evento que captura la palabra (ciudad) que se ingresa y con en el API de Google Geocode se hace una petición que nos retorna metadatos de la ubicación ingresada entre ellas las coordenadas.

--- foo.js
+++ foo.js
@@ -1,4 +1,15 @@
 // Create map
+var locationForm = document.getElementById('location-form');
+
+// Listen for submit
+locationForm.addEventListener('submit', geocode);
+
+function geocode(e){
+  e.preventDefault();
+	console.log("botonazo");
+	console.log(document.getElementById('location-input').value);
+}
+
 function initMap() {
   // Map options
   var options = {
@@ -8,13 +19,7 @@ function initMap() {
 
   // New map
   var map = new google.maps.Map(document.getElementById('map'), options);
-/*
-  // Listen for click on map
-  google.maps.event.addListener(map, 'click', function (event) {
-    // Add marker
-    addMarker({ coords: event.latLng });
-  });
-*/
+
 
   // Array of markers
   var markers = [
